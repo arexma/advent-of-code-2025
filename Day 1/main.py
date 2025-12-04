90fe5d5f5f1d893d6bd82d2478dcd9995da64fe4
arexma <maalexander2@yahoo.com>
2025-12-03 21:12:33 -0800

    Day 1, Puzzle 1 solution

---
+++ Day 1/main.py
@@ -0,0 +1,28 @@
+from utils.util import open_file
+
+def main():
+    contents = open_file('Day 1\input.txt')
+    
+    current = 50
+    zero_count = 0
+
+    for move in contents:
+        direction, steps = move[0], int(move[1:])
+
+        if direction == 'L':
+            current -= steps
+            if current < 0:
+                current = (100 * (abs(current) // 100)) + current
+
+        elif direction == 'R':
+            current += steps
+            if current > 99:
+                current = current - (100 * (current // 100))
+        
+        if current == 0:
+            zero_count += 1
+    
+    print("Password:", zero_count)
+
+if __name__ == '__main__':
+    main()
